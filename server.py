9b8c65ce2d8c878565587775054eaed49504e795
moebachrouch <moe.bachrouch@outlook.com>
2021-10-04 22:53:00 -0400

    added query parameter feature

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 import requests
 import random
 
@@ -10,8 +10,18 @@ def getRandomWord():
     word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
     response = requests.get(word_site)
     WORDS = response.content.splitlines()
+    WORDS = list(map(lambda word: word.decode('UTF-8'), WORDS))
+    # alternatively WORDS = [x.decode('utf-8') for x in WORDS]
 
-    randomWord = random.choice(WORDS).decode('UTF-8')
+    args = request.args
+    if "contains" in args:
+        contains = args["contains"]
+        WORDS = list(filter(lambda word: contains in word, WORDS))
+        if not WORDS:
+            WORDS = ["error"]
+        # alternatively WORDS = [word for word in WORDS if contains in word]
+
+    randomWord = random.choice(WORDS)
 
     return jsonify({"word": randomWord})
 
